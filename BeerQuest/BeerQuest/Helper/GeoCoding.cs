4fabcede9093ad16fc4c88bae5266cf644a79dce
Patrick <Patricknnoonan@gmail.com>
2019-06-17 18:24:30 -0500

    Added working link buttons to homepage carousel, seperated shared layouts and css from the homepage

--- BeerQuest/BeerQuest/Helper/GeoCoding.cs
+++ BeerQuest/BeerQuest/Helper/GeoCoding.cs
@@ -11,65 +11,66 @@ namespace BeerQuest.Helper
 {
     public class GoogleGeoCoding
     {
-        public static float[] GetLatLong(Stop stop)
+        //    public static float[] GetLatLong(Stop stop)
+        //    {
+        //        GoogleMap myMap = new GoogleMap();
+        //        string url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + stop.Address + ",+" +stop.City + ",+" + stop.State + "&key=" + ApiKeys.GoogleMapsApiKeys;
+        //        WebRequest requestObject = WebRequest.Create(url);
+        //        requestObject.Method = "GET";
+        //        HttpWebResponse responseObject = null;
+        //        responseObject = (HttpWebResponse)requestObject.GetResponse();
+
+        //        string urlResult = null;
+        //        using (Stream stream = responseObject.GetResponseStream())
+        //        {
+        //            StreamReader sr = new StreamReader(stream);
+        //            urlResult = sr.ReadToEnd();
+        //            sr.Close();
+        //        }
+
+        //        float lat;
+        //        float longitutde;
+
+        //        myMap = JsonConvert.DeserializeObject<GoogleMap>(urlResult);
+        //        try
+        //        {
+        //            lat = myMap.results[0].geometry.location.lat;
+        //            longitutde = myMap.results[0].geometry.location.lng;
+        //        }
+        //        catch
+        //        {
+        //            return null;
+        //        }
+        //        float[] coords = new float[2];
+        //        coords[0] = lat;
+        //        coords[1] = longitutde;
+        //        return coords;
+
+        //    }
+        //}
+
+        public class GoogleMap
         {
-            GoogleMap myMap = new GoogleMap();
-            string url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + stop.Address + ",+" +stop.City + ",+" + stop.State + "&key=" + ApiKeys.GoogleMapsApiKeys;
-            WebRequest requestObject = WebRequest.Create(url);
-            requestObject.Method = "GET";
-            HttpWebResponse responseObject = null;
-            responseObject = (HttpWebResponse)requestObject.GetResponse();
-
-            string urlResult = null;
-            using (Stream stream = responseObject.GetResponseStream())
-            {
-                StreamReader sr = new StreamReader(stream);
-                urlResult = sr.ReadToEnd();
-                sr.Close();
-            }
-
-            float lat;
-            float longitutde;
-
-            myMap = JsonConvert.DeserializeObject<GoogleMap>(urlResult);
-            try
-            {
-                lat = myMap.results[0].geometry.location.lat;
-                longitutde = myMap.results[0].geometry.location.lng;
-            }
-            catch
-            {
-                return null;
-            }
-            float[] coords = new float[2];
-            coords[0] = lat;
-            coords[1] = longitutde;
-            return coords;
-
+            public Result[] results { get; set; }
+            public string status { get; set; }
         }
-    }
 
-    public class GoogleMap
-    {
-        public Result[] results { get; set; }
-        public string status { get; set; }
-    }
+        public class Result
+        {
+            public Geometry geometry { get; set; }
+        }
 
-    public class Result
-    {
-        public Geometry geometry { get; set; }
-    }
+        public class Geometry
+        {
+            public Location location { get; set; }
+            public string location_type { get; set; }
+        }
 
-    public class Geometry
-    {
-        public Location location { get; set; }
-        public string location_type { get; set; }
-    }
-
-    public class Location
-    {
-        public float lat { get; set; }
-        public float lng { get; set; }
+        public class Location
+        {
+            public float lat { get; set; }
+            public float lng { get; set; }
+        }
     }
 }
 
