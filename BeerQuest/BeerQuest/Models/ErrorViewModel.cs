1f4c64b883f43b4b869a6387f52bd05090126508
Patrick <Patricknnoonan@gmail.com>
2019-06-17 10:16:45 -0500

    Repo init

---
+++ BeerQuest/BeerQuest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BeerQuest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
