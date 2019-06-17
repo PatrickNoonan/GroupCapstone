5fd2e6155fba70ac864ff839dcbf1dda7d751ac0
henry huynh <hlhuynh14@gmail.com>
2019-06-17 16:55:43 -0500

    made some view models for business controller

--- BeerQuest/BeerQuest/Models/Member.cs
+++ BeerQuest/BeerQuest/Models/Member.cs
@@ -8,14 +8,14 @@ namespace BeerQuest.Models
     public class Member
     {
         [Key]
-        public int id { get; set; }
+        public int Id { get; set; }
 
-        public string title { get; set; }
-        public double points { get; set; }
-        public string name { get; set; }
-        public List<Stop> stops{ get; set; }
-        [ForeignKey("ApplicationUser")]
-        public string ApplicationId { get; set; }
-        public ApplicationUser ApplicationUser { get; set; }
+        public string Title { get; set; }
+        public double Points { get; set; }
+        public string Name { get; set; }
+        public List<Stop> Stops{ get; set; }
+        //[ForeignKey("ApplicationUser")]
+        //public string ApplicationId { get; set; }
+        //public ApplicationUser ApplicationUser { get; set; }
     }
 }
