e376ba000119b440cf977ad636773ee9bd2bc609
Robert Smidt <rsmidt909@hotmail.com>
2019-06-18 14:32:21 -0500

    corrected error when finishedregistering that was invalid column name.

--- BeerQuest/Domain/Business.cs
+++ BeerQuest/Domain/Business.cs
@@ -24,7 +24,7 @@ namespace Domain
         public string ApplicationId { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
         public ApplicationRole ApplicationRole { get; set; }
-        public string Role { get; set; }
+        public string UserRole { get; set; }
 
     }
 }
