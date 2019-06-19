43e31873d9e3d661feedf2e1aeebc411229d5b8b
henry huynh <hlhuynh14@gmail.com>
2019-06-19 07:47:36 -0500

    pulling

--- BeerQuest/BeerQuest/Controllers/HomeController.cs
+++ BeerQuest/BeerQuest/Controllers/HomeController.cs
@@ -39,5 +39,6 @@ namespace BeerQuest.Controllers
         {
             return View(new Domain.ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        
     }
 }
--- BeerQuest/Infrastructure/Migrations/20190618181207_13377.cs
+++ BeerQuest/Infrastructure/Migrations/20190618181207_13377.cs
@@ -9,7 +9,7 @@ namespace Infrastructure.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             
-            migrationBuilder.Sql("SET Identity_Insert Teams ON INSERT INTO Teams (Id, Name) VALUES (2, 'Dallas Cowboys')");
+          
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
