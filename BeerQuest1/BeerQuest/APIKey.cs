9f2321ecd4624e4e04d249f194d04a80f237ebb3
Robert Smidt <rsmidt909@hotmail.com>
2019-06-20 09:08:06 -0500

    case fix

---
+++ BeerQuest1/BeerQuest/APIKey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeerQuest
+{
+    public class APIKey
+    {
+        private static string secretKey = "AIzaSyAud5xYFvsXHMlKE7mco_D8K6kxInw5jMM";
+        public static string SecretKey { get { return secretKey; } }
+    }
+}
