4740e6b0f09f00e917fa435a3b4cafa4adda23f0
Patrick <Patricknnoonan@gmail.com>
2019-06-22 21:29:54 -0500

    transferred bar graph scripts from inline to a link to js file

---
+++ BeerQuest1/BeerQuest/wwwroot/js/BusinessSiteLineGraph.js
@@ -0,0 +1,24 @@
+﻿let width = window.innerWidth * 0.8 // svg width
+let height = 400 // svg height
+let padding = 80
+let months = [
+    'Jan', 'Feb', 'Mar', 'Apr', 'May',
+    'Jun', 'Jul', 'Aug', 'Sep',
+    'Oct', 'Nov', 'Dec'
+];
+
+// data
+let monthlyvisits = [
+    { "month": 1, "visits": 100 },
+    { "month": 2, "visits": 130 },
+    { "month": 3, "visits": 250 },
+    { "month": 4, "visits": 300 },
+    { "month": 5, "visits": 265 },
+    { "month": 6, "visits": 225 },
+    { "month": 7, "visits": 180 },
+    { "month": 8, "visits": 120 },
+    { "month": 9, "visits": 145 },
+    { "month": 10, "visits": 130 },
+    { "month": 11, "visits": 80 },
+    { "month": 12, "visits": 40 }
+]
