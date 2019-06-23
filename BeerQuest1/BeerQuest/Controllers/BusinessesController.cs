666e71914c318eb34a3764f33a9b6e02e2861b5a
henry huynh <hlhuynh14@gmail.com>
2019-06-23 17:42:55 -0500

    new getpiedata method

--- BeerQuest1/BeerQuest/Controllers/BusinessesController.cs
+++ BeerQuest1/BeerQuest/Controllers/BusinessesController.cs
@@ -237,5 +237,12 @@ namespace BeerQuest.Controllers
             return loggedInBusiness;
 
         }
+        public ActionResult GetPieData()
+        {
+            List<Message> data = new List<Message>();
+            Business currentBusiness = GetLoggedInBusiness();
+             data = _context.Messages.Where(c => c.CurrentBar == currentBusiness.Name).ToList();          
+            return Json(data);
+        }
     }
 }
