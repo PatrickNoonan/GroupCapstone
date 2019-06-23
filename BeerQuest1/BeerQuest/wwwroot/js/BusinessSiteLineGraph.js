884c1a329c522011efb7be84ad46d8e8072cb16a
Patrick <Patricknnoonan@gmail.com>
2019-06-22 21:42:03 -0500

    Started logic for working with data in yearly line graph

--- BeerQuest1/BeerQuest/wwwroot/js/BusinessSiteLineGraph.js
+++ BeerQuest1/BeerQuest/wwwroot/js/BusinessSiteLineGraph.js
@@ -1,24 +1,30 @@
-﻿let width = window.innerWidth * 0.8 // svg width
-let height = 400 // svg height
-let padding = 80
-let months = [
-    'Jan', 'Feb', 'Mar', 'Apr', 'May',
-    'Jun', 'Jul', 'Aug', 'Sep',
-    'Oct', 'Nov', 'Dec'
-];
+﻿$.ajax({
+    url: '/Businesses/GetData',
+    data: "",
+    dataType: "json",
+    type: "POST",
+    contentType: "application/json; chartset=utf-8",
+})
+    .done(function (data) {
+        //data brings back a date and a number of visits for that date
 
-// data
-let monthlyvisits = [
-    { "month": 1, "visits": 100 },
-    { "month": 2, "visits": 130 },
-    { "month": 3, "visits": 250 },
-    { "month": 4, "visits": 300 },
-    { "month": 5, "visits": 265 },
-    { "month": 6, "visits": 225 },
-    { "month": 7, "visits": 180 },
-    { "month": 8, "visits": 120 },
-    { "month": 9, "visits": 145 },
-    { "month": 10, "visits": 130 },
-    { "month": 11, "visits": 80 },
-    { "month": 12, "visits": 40 }
-]
+        let width = window.innerWidth * 0.8 // svg width
+        let height = 400 // svg height
+        let padding = 80
+        let months = [
+            'Jan', 'Feb', 'Mar', 'Apr', 'May',
+            'Jun', 'Jul', 'Aug', 'Sep',
+            'Oct', 'Nov', 'Dec'
+        ];
+        let monthlyVisits = [];
+        for (let i = 0; i < data.length; i++) {
+            let count = 0;
+            if (data[i].date.getMonth() == Jan)
+                switch (month) {
+                    case '01'
+                }
+        }
+
+        
+    });
+
